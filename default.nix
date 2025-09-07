dc7d7d8581eb1d6fc1ff4acf5c9a504c0f6faa93
Sinedka <123822966+Sinedka@users.noreply.github.com>
2025-09-08 00:46:09 +0300

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,45 +1,27 @@
-{
-  rev,
-  lib,
-  python3,
-  installShellFiles,
-  swappy,
-  libnotify,
-  slurp,
-  wl-clipboard,
-  cliphist,
-  app2unit,
-  dart-sass,
-  grim,
-  fuzzel,
-  wl-screenrec,
-  dconf,
-  killall,
-  caelestia-shell,
-  withShell ? false,
-  discordBin ? "discord",
-  qtctStyle ? "Darkly",
-}:
+{ rev, lib, python3, installShellFiles, swappy, libnotify, slurp, wl-clipboard,
+  cliphist, app2unit, dart-sass, grim, fuzzel, wl-screenrec, dconf, killall,
+  caelestia-shell, withShell ? false, discordBin ? "discord", qtctStyle ? "Darkly" }:
 
-python3.pkgs.buildPythonApplication {
+python3.pkgs.buildPythonApplication rec {
   pname = "caelestia-cli";
   version = rev;
   src = ./.;
   pyproject = true;
 
-  build-system = with python3.pkgs; [
-    hatch-vcs
-    hatchling
-  ];
+  # Build system (Hatch)
+  build-system = with python3.pkgs; [ hatch-vcs hatchling ];
 
-  dependencies = with python3.pkgs; [
+  # Python dependencies
+  propagatedBuildInputs = with python3.pkgs; [
     materialyoucolor
     pillow
   ];
 
+  # Проверка импортов Python
   pythonImportsCheck = ["caelestia"];
 
-  nativeBuildInputs = [installShellFiles];
+  # Системные зависимости
+  nativeBuildInputs = [ installShellFiles ];
   propagatedBuildInputs = [
     swappy
     libnotify
@@ -55,44 +37,41 @@ python3.pkgs.buildPythonApplication {
     killall
   ] ++ lib.optional withShell caelestia-shell;
 
+  # Переменная для setuptools_scm
   SETUPTOOLS_SCM_PRETEND_VERSION = 1;
 
+  # Патчи для скриптов и конфигов
   patchPhase = ''
-    # Replace qs config call with nix shell pkg bin
     substituteInPlace src/caelestia/subcommands/shell.py \
-    	--replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
+      --replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
     substituteInPlace src/caelestia/subcommands/screenshot.py \
-    	--replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
-
-    # Use config bin instead of discord + fix todoist
+      --replace-fail '"qs", "-c", "caelestia"' '"caelestia-shell"'
     substituteInPlace src/caelestia/subcommands/toggle.py \
-    	--replace-fail 'discord' ${discordBin} \
+      --replace-fail 'discord' ${discordBin} \
       --replace-fail 'todoist' 'todoist.desktop'
-
-    # Use config style instead of darkly
     substituteInPlace src/caelestia/data/templates/qtct.conf \
-    	--replace-fail 'Darkly' '${qtctStyle}'
+      --replace-fail 'Darkly' '${qtctStyle}'
   '';
 
-  # --- Новый installPhase для автоматической установки бинарника ---
-  installPhase = ''
-    # стандартная установка Python приложения
-    python3 -m pip install . --prefix=$out
-
-    # убедимся, что исполняемый файл появился в $out/bin
-    mkdir -p $out/bin
-    cp $out/lib/python*/site-packages/caelestia/cli.py $out/bin/caelestia
-    chmod +x $out/bin/caelestia
-
-    # установим completions
+  # Пост-инсталляция
+  postInstall = ''
     installShellCompletion completions/caelestia.fish
   '';
+
+  # Важно: делаем entry point доступным через bin
+  doInstallCheck = true;
+  installTargets = [ "bin" ];
+
+  # Этот блок гарантирует, что console_scripts из pyproject.toml попадут в bin/
+  # Если в pyproject.toml прописан entry point:
+  # [project.scripts]
+  # caelestia = "caelestia.__main__:main"
+  # то он будет установлен автоматически в result/bin/caelestia
 
   meta = {
     description = "The main control script for the Caelestia dotfiles";
     homepage = "https://github.com/caelestia-dots/cli";
     license = lib.licenses.gpl3Only;
-    mainProgram = "caelestia";
     platforms = lib.platforms.linux;
   };
 }
